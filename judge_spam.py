29010b9298f7775691d09fac9251f7e7392ca17c
yusuke1565 <yukke_in-o157@i.softbank.jp>
2019-11-26 17:57:04 +0900

    add scorer

---
+++ judge_spam.py
@@ -0,0 +1,81 @@
+#python kadai7_2.py test.txt
+#judge spam mail
+
+def make_L2pL(file):  #pS,pN
+    with open(file, "r") as f:
+        L2pL = {}
+        for line in f:
+            line = line.rstrip()
+            LW = line.split(" ")
+            L2pL[LW[0]] = LW[1]
+    return L2pL
+
+
+def make_L2w2pw(file):  #p(w|S),p(w|N),...
+    with open(file,"r") as f:
+        L2w2pw={}
+        for line in f:
+            line = line.rstrip()
+            LWPw = line.split(" ")
+            L2w2pw[LWPw[0]] = L2w2pw.get(LWPw[0],{}) #define two dimentions dictionary
+            L2w2pw[LWPw[0]][LWPw[1]] = LWPw[2]
+    return L2w2pw
+
+
+def countWord(line):  #return word2freq
+    w2f={}
+    line = line.rstrip()
+    line = mecab.parse(line)
+    words = line.split(" ")
+    for word in words:
+        w2f[word] = w2f.get(word,0) +1
+    return w2f
+
+
+def calculate_p(test_w2f, pL, w2pw):  #calculate probability to judge mail
+    ans = math.log(float(pL))
+    for word in test_w2f.keys():
+        ans = ans + math.log( float(w2pw.get(word,0.000000001)) ) * test_w2f[word]
+    return ans
+
+
+
+import sys
+import MeCab
+import math
+import kadai7_1
+
+args = sys.argv
+mecab = MeCab.Tagger('-Owakati')
+file = args[1]
+
+L2pL = make_L2pL("pL.txt")
+L2w2pw = make_L2w2pw("pw_L.txt")
+
+Ls=[]
+mailLen=0
+match=0
+with open(file,"r") as file:
+    for line in file:
+        mailLen+=1
+        test_L=kadai7_1.return_Label(line)
+        test_w2f = {}
+        test_w2f = countWord(line)
+
+        L2p={}
+        for L in L2pL.keys():
+            L2p[L] = calculate_p(test_w2f, L2pL[L], L2w2pw[L])
+
+        a=-9999999999999
+        judge=0
+        for L in L2p.keys():
+            if a < L2p[L]:
+                a = L2p[L]
+                judge = L
+
+        if judge == test_L:
+            match+=1
+
+        print(judge)
+
+    print("precision:" + str(match) + "/" + str(mailLen))
