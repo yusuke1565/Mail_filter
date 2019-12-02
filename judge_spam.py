5b8e4b54368e27360a1eae506a4e0d584a809615
yusuke1565 <yukke_in-o157@i.softbank.jp>
2019-12-02 14:11:31 +0900

    add score

--- judge_spam.py
+++ judge_spam.py
@@ -1,7 +1,16 @@
 #python kadai7_2.py test.txt
 #judge spam mail
 
-def make_L2pL(file):  #pS,pN
+import sys
+import MeCab
+import math
+import calculate_probability
+
+args = sys.argv
+mecab = MeCab.Tagger('-Owakati')
+
+
+def make_Label2pL(file):  #pS,pN,...
     with open(file, "r") as f:
         L2pL = {}
         for line in f:
@@ -11,7 +20,7 @@ def make_L2pL(file):  #pS,pN
     return L2pL
 
 
-def make_L2w2pw(file):  #p(w|S),p(w|N),...
+def make_Label2word2pw(file):  #p(w|S),p(w|N),...
     with open(file,"r") as f:
         L2w2pw={}
         for line in f:
@@ -22,60 +31,47 @@ def make_L2w2pw(file):  #p(w|S),p(w|N),...
     return L2w2pw
 
 
-def countWord(line):  #return word2freq
-    w2f={}
-    line = line.rstrip()
-    line = mecab.parse(line)
-    words = line.split(" ")
-    for word in words:
-        w2f[word] = w2f.get(word,0) +1
-    return w2f
-
-
-def calculate_p(test_w2f, pL, w2pw):  #calculate probability to judge mail
+def calculate_prob(test_w2f, pL, w2pw):  #calculate probability to judge mail
     ans = math.log(float(pL))
     for word in test_w2f.keys():
         ans = ans + math.log( float(w2pw.get(word,0.000000001)) ) * test_w2f[word]
     return ans
 
 
+def main():
+    file = args[1]
 
-import sys
-import MeCab
-import math
-import kadai7_1
+    L2pL = make_Label2pL("pL.txt")
+    L2w2pw = make_Label2word2pw("pw_L.txt")
 
-args = sys.argv
-mecab = MeCab.Tagger('-Owakati')
-file = args[1]
 
-L2pL = make_L2pL("pL.txt")
-L2w2pw = make_L2w2pw("pw_L.txt")
+    mailLen=0
+    match=0
+    with open(file,"r") as file:
+        for line in file:
+            mailLen+=1
+            test_L , test_sentence = calculate_probability.detach_Label(line)
+            test_w2f = calculate_probability.make_word2freq(test_sentence)
 
-Ls=[]
-mailLen=0
-match=0
-with open(file,"r") as file:
-    for line in file:
-        mailLen+=1
-        test_L=kadai7_1.return_Label(line)
-        test_w2f = {}
-        test_w2f = countWord(line)
+            L2p={}
+            for L in L2pL.keys():
+                L2p[L] = calculate_prob(test_w2f, L2pL[L], L2w2pw[L])
 
-        L2p={}
-        for L in L2pL.keys():
-            L2p[L] = calculate_p(test_w2f, L2pL[L], L2w2pw[L])
+            a=-99999999
+            judge=0
+            for L in L2p.keys():
+                if a < L2p[L]:
+                    a = L2p[L]
+                    judge = L
 
-        a=-9999999999999
-        judge=0
-        for L in L2p.keys():
-            if a < L2p[L]:
-                a = L2p[L]
-                judge = L
+            if judge == test_L:
+                match+=1
 
-        if judge == test_L:
-            match+=1
+            print(judge)
 
-        print(judge)
+        print("precision:" + str(match) + "/" + str(mailLen))
 
-    print("precision:" + str(match) + "/" + str(mailLen))
+
+
+if __name__ == "__main__":
+    main()
